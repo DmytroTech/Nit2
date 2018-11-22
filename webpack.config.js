94b9b333b0ea4f3c7cc5f4defe9698654a642b8d
FirstOfAll <dimdor91.dd@gmail.com>
2018-11-22 19:11:30 +0200

    Added es6 + ended Babel task

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,20 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'js/main.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            // ...additional rules...
+        ],
+    },
 };
