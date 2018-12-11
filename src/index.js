4181c4e0889fc1781a71342e9953adeb3c78033f
Dmytro Doroshenko <dimdor91.dd@gmail.com>
2018-12-11 23:56:28 +0200

    Images fixed

--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
-let navCatagory = require('./modules/category-html');
+let AllProducts = require('./modules/product-grid');
+let navCatagory = require('./modules/nav-categorys');
+
 jQuery.ajax({
     url: 'https://nit.tron.net.ua/api/category/list',
     method: 'get',
     dataType: 'json',
     success: function(json){
-        console.table(json);
         json.forEach(product => $('.navbar-nav').append(navCatagory(product)));
+    },
+});
+
+
+
+jQuery.ajax({
+    url: 'https://nit.tron.net.ua/api/product/list',
+    method: 'get',
+    dataType: 'json',
+    success: function(json){
+        $('.product-grid').empty();
+        json.forEach(product => $('.product-grid').append(AllProducts(product)));
     },
     error: function(xhr){
         alert("An error occured: " + xhr.status + " " + xhr.statusText);
