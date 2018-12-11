0a8f452b7f4a17ffcd11ae72da447643509e5284
Dmytro Doroshenko <dimdor91.dd@gmail.com>
2018-12-11 20:21:23 +0200

    Categories done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './scss/main.scss';
+import 'bootstrap';
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 
-console.log('Hi');
-console.log(`The time is ${new Date()}`);
+let navCatagory = require('./modules/category-html');
+jQuery.ajax({
+    url: 'https://nit.tron.net.ua/api/category/list',
+    method: 'get',
+    dataType: 'json',
+    success: function(json){
+        console.table(json);
+        json.forEach(product => $('.navbar-nav').append(navCatagory(product)));
+    },
+    error: function(xhr){
+        alert("An error occured: " + xhr.status + " " + xhr.statusText);
+    },
+});
